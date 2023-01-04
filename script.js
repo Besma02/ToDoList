43a07dc8ee151a65b8a460da0e0a4995c450c3f2
besmaharzli <besmaharzli@gmail.com>
2023-01-04 19:13:25 +0100

    todolist

--- script.js
+++ script.js
@@ -1,7 +1,60 @@
-const button=document.querySelector('button')
-button.addEventListener('click',ToDoList)
-function ToDoList(){
-    const l1=document.create('div')
-    l1.innerH
-}
+const button=document.getElementById('b1');
+
+button.addEventListener("click", function(){
+    const trash=document.createElement('trash')
+    trash.innerHTML='<i class="fa fa-trash"></i>'
+    const div= document.createElement('div')
+    const input=document.getElementById('input')
+    const newtext = document.createTextNode(input.value);
+     const elem=div.appendChild(newtext)
+    div.appendChild(trash)
+    trash.classList.add("delete")
+
+    const tasks=document.getElementById('tasks')
+    tasks.appendChild(div);
+    div.classList.add("best")
+    const table=document.getElementsByClassName('delete')
+    for(let i=0;i<table.length;i++){
+        table[i].onclick=function(){
+        this.parentNode.remove()};
+        
+    }
+    
+
+});
+
+
+
+const b2=document.getElementById('b2');
+
+b2.addEventListener("click", function(){
+  
+    document.getElementById('tasks').innerHTML=""
+
+})
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
+    
+
+
+ 
+ 
+
 
